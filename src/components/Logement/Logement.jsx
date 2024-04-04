0f7c6e9cef936062067e0f3bff0b2091580625f7
Paul Ledieu <paul.ledieu@gmail.com>
2024-04-04 14:34:06 +0200

    redirection into 2 files in Logement's Component

--- src/components/Logement/Logement.jsx
+++ src/components/Logement/Logement.jsx
@@ -1,76 +1,13 @@
-import Card from '../Card/Card.jsx'
-import Carrousel from '../Carrousel/Carrousel.jsx'
-import Tag from '../Tag/Tag.jsx'
-import Rating from '../Rating/Rating.jsx'
-import Collapse from '../Collapse/Collapse.jsx'
-import AuthorCard from '../AuthorCard/AuthorCard.jsx'
-
-import './Logement.css'
-
+import Card from './logementCard/Card.jsx';
+import LogementDetail from './logementDetail/logementDetail.jsx'; // Import de la nouvelle composante
+import './Logement.css';
 
 const Logement = ({ data, mode }) => {
-
     if (mode === 'card') {
-        return <Card data={data} />
+        return <Card data={data} />;
     } else if (mode === 'detail') {
-        return (
-            <>
-                <header className="detail-header">
-                    <div className="carrousel-container">
-                        <Carrousel pictures={data.pictures} />
-                    </div>
-                </header>
-                <main className="detail-main">
-                    <section className="info-container">
-                        <div className="title-container">
-                            <h1>{data.title}</h1>
-                            <h2>{data.location}</h2>
-                            <div className="tag-container">
-                                <Tag tags={data.tags} />
-                            </div>
-                        </div>
-                        <div className="rating-author-container">
-                            <div className="author-card-container">
-                                <AuthorCard
-                                    key={`${data.id}-name`}
-                                    picture={data.host.picture}
-                                    name={data.host.name}
-                                />
-                            </div>
-
-                            <div className="rating-container">
-                                <Rating rating={data.rating} />
-                            </div>
-                        </div>
-                    </section>
-                    <div className="detail-collapse-container">
-                        <Collapse
-                            key={`${data.id}-description`}
-                            title="Description"
-                            content={data.description}
-                        />
-                        <Collapse
-                            key={`${data.id}-equipments`}
-                            title="Équipements"
-                            content={
-                                <ul>
-                                    {data.equipments.map(
-                                        (equipment, index) => (
-                                            <li
-                                                key={`${data.id}-equipment-${index}`}
-                                            >
-                                                {equipment}
-                                            </li>
-                                        )
-                                    )}
-                                </ul>
-                            }
-                        />
-                    </div>
-                </main>
-            </>
-        )
+        return <LogementDetail data={data} />;
     }
-}
+};
 
-export default Logement
+export default Logement;
---
+++ src/components/Logement/logementDetail/logementDetail.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Carrousel from '../../Carrousel/Carrousel.jsx';
+import Tag from '../../Tag/Tag.jsx';
+import Rating from '../../Rating/Rating.jsx';
+import Collapse from '../../Collapse/Collapse.jsx';
+import AuthorCard from '../../AuthorCard/AuthorCard.jsx';
+
+const LogementDetail = ({ data }) => {
+    return (
+        <>
+            <header className="detail-header">
+                <div className="carrousel-container">
+                    <Carrousel pictures={data.pictures} />
+                </div>
+            </header>
+            <main className="detail-main">
+                <section className="info-container">
+                    <div className="title-container">
+                        <h1>{data.title}</h1>
+                        <h2>{data.location}</h2>
+                        <div className="tag-container">
+                            <Tag tags={data.tags} />
+                        </div>
+                    </div>
+                    <div className="rating-author-container">
+                        <div className="author-card-container">
+                            <AuthorCard
+                                key={`${data.id}-name`}
+                                picture={data.host.picture}
+                                name={data.host.name}
+                            />
+                        </div>
+
+                        <div className="rating-container">
+                            <Rating rating={data.rating} />
+                        </div>
+                    </div>
+                </section>
+                <div className="detail-collapse-container">
+                    <Collapse
+                        key={`${data.id}-description`}
+                        title="Description"
+                        content={data.description}
+                    />
+                    <Collapse
+                        key={`${data.id}-equipments`}
+                        title="Équipements"
+                        content={
+                            <ul>
+                                {data.equipments.map((equipment, index) => (
+                                    <li key={`${data.id}-equipment-${index}`}>
+                                        {equipment}
+                                    </li>
+                                ))}
+                            </ul>
+                        }
+                    />
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default LogementDetail;
